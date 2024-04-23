a87de3a18c2b45b8e74a99f6008173826384a9c7
mnikhwan <kibaro.kun21@gmail.com>
2024-04-24 05:43:40 +0700

    Menyala🔥

---
+++ app/src/main/java/com/unisri/storeAPP/MainActivity.java
@@ -0,0 +1,24 @@
+package com.unisri.storeAPP;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWeb;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWeb = findViewById(R.id.myWeb);
+        myWeb.getSettings().setJavaScriptEnabled(true);
+        myWeb.setWebViewClient(new WebViewClient());
+        myWeb.loadUrl("https://unisristore.com/");
+    }
+}
