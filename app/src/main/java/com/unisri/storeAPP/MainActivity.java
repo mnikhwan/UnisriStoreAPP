24bb702bca70ae816ffdfb438f7231a25f0526f3
mnikhwan <kibaro.kun21@gmail.com>
2024-05-05 11:46:56 +0700

    add navbottom🔥

--- app/src/main/java/com/unisri/storeAPP/MainActivity.java
+++ app/src/main/java/com/unisri/storeAPP/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.webkit.URLUtil;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,25 +31,31 @@ public class MainActivity extends AppCompatActivity {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 // This method is deprecated in API 24, but still needed for lower API levels
                 if (URLUtil.isNetworkUrl(url)) {
-//                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-//                    MainActivity.this.startActivity(Intent.createChooser(intent, "Choose browser"));
-                    return false;
-                }
-                if (appInstalledOrNot(url)) {
-                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                    MainActivity.this.startActivity(Intent.createChooser(intent, "Choose browser")); // Corrected here
-                    //MainActivity.this.startActivity(intent); // Corrected here
-                } else {
-                    Toast.makeText(MainActivity.this, "Whatsapp or Instagram NOT Installed", Toast.LENGTH_SHORT).show();
+                    if(url.contains("unisristore.com")){
+                        return false;
+                    } else if (url.contains("instagram")) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        MainActivity.this.startActivity(Intent.createChooser(intent, "Choose browser"));
+                        Toast.makeText(MainActivity.this, "Beli via Instagram", Toast.LENGTH_SHORT).show();
+                    } else if (url.contains("wa") && url.contains("wa.me") && url.contains("whatsapp")) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        MainActivity.this.startActivity(Intent.createChooser(intent, "Choose browser"));
+                        Toast.makeText(MainActivity.this, "Beli via Whatsapp", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        MainActivity.this.startActivity(Intent.createChooser(intent, "Choose browser"));
+                        Toast.makeText(MainActivity.this, "Opening Browser", Toast.LENGTH_SHORT).show();
+                    }
                 }
                 return true;
             }
-
-
         });
-
         myWeb.loadUrl("https://unisristore.com/");
     }
+
+
+
 
     private boolean appInstalledOrNot(String uri) {
         PackageManager pm = getPackageManager();
@@ -60,3 +67,10 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 }
+//                if (appInstalledOrNot(url)) {
+//                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+//                    MainActivity.this.startActivity(Intent.createChooser(intent, "Choose browser")); // Corrected here
+//                    //MainActivity.this.startActivity(intent); // Corrected here
+//                } else {
+//                  Toast.makeText(MainActivity.this, "Whatsapp or Instagram NOT Installed", Toast.LENGTH_SHORT).show();
+//                }
